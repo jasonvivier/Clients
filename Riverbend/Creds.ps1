9011c813f7668978515c9d14a0dee859eb3b300f
jasonvivier <jason.a.vivier@gmail.com>
2015-11-30 10:03:16 -0500

    Push

---
+++ Riverbend/Creds.ps1
@@ -0,0 +1,22 @@
+﻿69Mu$tangMach!
+
+Amoskeag
+@ncg4M3
+
+River2 - 192.168.220.22        FSMO, Primary DC, Primary WINS, SEPM
+River1 - 192.168.220.23        DC, File Server, Secondary DNS
+TS1 - 192.168.220.21            Domain Member, Terminal Server
+ 
+For the new servers, please use the following IP's:
+ 
+RB-RB-HYPER1   192.168.220.16 /24
+RB-HYPER2          192.168.220.17 /24
+River-1                   192.168.220.25 /24      
+River-2                   192.168.220.27 /24
+TS-1                       192.168.220.28 /24
+ 
+DG                          192.168.220.4
+ 
+Secondary WIN's is 207.136.243.10
+
+
